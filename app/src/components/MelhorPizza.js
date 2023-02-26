16f597d5d6078076aae161739a1cdada53f8e58c
Erick Bueno <101439440+Erick-Bueno@users.noreply.github.com>
2023-02-26 13:19:20 -0300

    Update MelhorPizza.js

--- app/src/components/MelhorPizza.js
+++ app/src/components/MelhorPizza.js
@@ -5,7 +5,7 @@ export default function MelhorPizza(){
         <section className={styles.fundo} >
             <article className={styles.contFundo}>
                 <h1 className={styles2.fonte}>MELHOR <span className={styles2.sabor}>PIZZA</span> DA CIDADE</h1>
-                <p className={styles.pa}>NOSSA COMIDA E FRESCA E SIMPLES. AS PIZZAS SÃO FEITAS TODA MANHÃ.TODOS OS NOSSOS RECHEIOS TAMBEM SÃO PREPARADOS DIARIAMENTE COM PRODUTOS DE EXTREMA QUALIDADE DOS MELHORES MERCADOS POSSIVEIS. QUANDO VOCÊ VISITAR OU PEDIR ONLINE. SUA PIZZA VAI SER PREPARADA POR UMA PESSOA DE EXTREMA TECNICA E EXCELÊNCIA NA COZINHHA.</p>
+                <p className={styles.pa}>NOSSA COMIDA É FRESCA E SIMPLES. AS PIZZAS SÃO FEITAS TODA MANHÃ.TODOS OS NOSSOS RECHEIOS TAMBEM SÃO PREPARADOS DIARIAMENTE COM PRODUTOS DE EXTREMA QUALIDADE DOS MELHORES MERCADOS POSSIVEIS. QUANDO VOCÊ VISITAR OU PEDIR ONLINE, SUA PIZZA VAI SER PREPARADA POR UMA PESSOA DE EXTREMA TÉCNICA E EXCELÊNCIA NA COZINHHA.</p>
                 <section className={styles2.contbtn}><button className={styles2.comprar}>COMPRAR AGORA</button> <span className={styles2.contSeta}><button className={`${styles2.verMenu} ${styles.vermenu2}`}>VER O MENU</button> <img className={styles2.seta} src='/images/seta.png' width={15}/></span></section>
        
             </article>
